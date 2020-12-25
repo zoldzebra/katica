124ca30e880926bb3650635247e7c15c0dd50648
zoldzebra <zoldzebra@gmail.com>
2020-12-25 17:33:18 +0100

    Set up manual lint script

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 // import { LobbyComponent } from './components/Lobby_old';
 import { Lobby } from './components/Lobby/Lobby';
 
+
 const App = () => {
   return (
     <Lobby />
