c7165be3837ae2e3d73b62b25e8038e6946f1663
zoldzebra <zoldzebra@gmail.com>
2020-12-27 17:19:52 +0100

    Display username and email in Lobby

--- src/components/Lobby/Lobby.tsx
+++ src/components/Lobby/Lobby.tsx
@@ -1,15 +1,19 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import { Paper } from '@material-ui/core';
 import { useHistory } from "react-router-dom";
 
-import firebaseApp from "../../Firebase/firebaseApp";
+import firebase from "firebase/app";
+import "firebase/auth";
 import "firebase/firestore";
 import { LobbyClient } from 'boardgame.io/client';
 import { LobbyAPI } from 'boardgame.io';
 
+import firebaseApp from "../../Firebase/firebaseApp";
 import { APP_PRODUCTION, LOCAL_SERVER_URL } from '../../config';
+import { AuthContext } from '../../Firebase/FireBaseAuthProvider';
 
 export const Lobby = (): JSX.Element => {
+  const { user } = useContext(AuthContext);
   const { protocol, hostname, port } = window.location;
   const history = useHistory();
   const server = APP_PRODUCTION
@@ -17,21 +21,50 @@ export const Lobby = (): JSX.Element => {
     : LOCAL_SERVER_URL;
   const lobbyClient = new LobbyClient({ server });
 
+  const [userInfo, setUserInfo] = useState<firebase.firestore.DocumentData | undefined>({
+    email: '',
+    username: '',
+  });
   const [games, setGames] = useState<string[]>([]);
   const [matches, setMatches] = useState<LobbyAPI.Match[]>([]);
 
   const handleLogoutClick = (event: any) => {
     event.preventDefault();
-
     firebaseApp
       .auth()
       .signOut()
-      .then(res => {
-        console.log(res);
+      .then(() => {
         history.push("/auth/login");
       })
   }
 
+  useEffect(() => {
+    const fetchUserInfo = async (): Promise<void> => {
+      const db = firebase.firestore();
+      const userInfoRef = db.collection("Users").doc(user?.uid);
+      userInfoRef
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            console.log("Document data:", doc.data());
+            setUserInfo(doc.data());
+          } else {
+            console.log("No such document!");
+          }
+        }).catch(function (error) {
+          console.log("Error getting document:", error);
+        });
+    }
+    fetchUserInfo();
+  }, []);
+
+  useEffect(() => {
+    const fetchGames = async (): Promise<void> => {
+      const gamesList = await lobbyClient.listGames();
+      setGames(gamesList);
+    }
+    fetchGames();
+  }, []);
 
   useEffect(() => {
     const fetchGames = async (): Promise<void> => {
@@ -51,9 +84,10 @@ export const Lobby = (): JSX.Element => {
 
   return (
     <Paper>
+      <p>Username: {userInfo?.username}, email: {userInfo?.email}</p>
+      <button onClick={handleLogoutClick}>Logout</button>
       <h1>Katica Lobby</h1>
       <p>Welcome to the Lobby!</p>
-      <button onClick={handleLogoutClick}>Logout</button>
       <p>Available games:</p>
       <ul>
         {games.map(game => <li key={game}>{game}</li>)}
