a5893c316eed94046eb69e373f6a71e813bfc0a0
zoldzebra <zoldzebra@gmail.com>
2021-01-05 21:18:24 +0100

    Fix gameServer address passing, use context

--- src/config.js
+++ src/config.js
@@ -1,7 +1,1 @@
-const LOCAL_SERVER_PORT = 8000;
-const LOCAL_SERVER_URL = `http://localhost:${LOCAL_SERVER_PORT}`;
-const APP_PRODUCTION = process.env.NODE_ENV === 'production';
-const { protocol, hostname, port } = window.location;
-export const server = APP_PRODUCTION
-  ? `${protocol}//${hostname}:${port}`
-  : LOCAL_SERVER_URL;
+export const LOCAL_SERVER_PORT = 8000;
