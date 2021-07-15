14d1b90657c815b49740e3bc1dd5fdf997131baf
zoldzebra <zoldzebra@gmail.com>
2021-07-15 21:37:36 +0200

    Pick advantage pieces with seeded random

--- src/Games/Katica/GameAdvantageBoardMutations.ts
+++ src/Games/Katica/GameAdvantageBoardMutations.ts
@@ -2,7 +2,12 @@ import { Piece } from './GameCreateStartingBoard';
 import { EMPTY_FIELD } from './GameConstants';
 
 
-// use class?
+interface PlayerPieces {
+  pieceIndex: number,
+  piece: Piece,
+}
+
+// TODO: use class to maintain state of playerPiecesBoard and to reduce code duplication...
 export function advantageBoardLevel1(board: Piece[], color: number): Piece[] {
   const playerPiecesBoard = createPlayerPiecesBoard(board, color);
 
@@ -37,13 +42,11 @@ export function advantageBoardLevel3(board: Piece[], color: number): Piece[] {
   const piece1Index = findIndexAndRemove(playerPiecesBoard, 1);
   const piece2Index = findIndexAndRemove(playerPiecesBoard, 2);
   const piece3Index = findIndexAndRemove(playerPiecesBoard, 3);
-
   const piece4Index = findIndexAndRemove(playerPiecesBoard, 3);
 
   [board[27], board[piece1Index]] = [board[piece1Index], board[27]];
   [board[26], board[piece2Index]] = [board[piece2Index], board[26]];
   [board[21], board[piece3Index]] = [board[piece3Index], board[21]];
-
   [board[20], board[piece4Index]] = [board[piece4Index], board[20]];
 
   return board;
@@ -55,13 +58,11 @@ export function advantageBoardLevel4(board: Piece[], color: number): Piece[] {
   const piece1Index = findIndexAndRemove(playerPiecesBoard, 1);
   const piece2Index = findIndexAndRemove(playerPiecesBoard, 2);
   const piece3Index = findIndexAndRemove(playerPiecesBoard, 2);
-
   const piece4Index = findIndexAndRemove(playerPiecesBoard, 3);
 
   [board[27], board[piece1Index]] = [board[piece1Index], board[27]];
   [board[26], board[piece2Index]] = [board[piece2Index], board[26]];
   [board[21], board[piece3Index]] = [board[piece3Index], board[21]];
-
   [board[20], board[piece4Index]] = [board[piece4Index], board[20]];
 
   return board;
@@ -74,38 +75,61 @@ export function advantageBoardLevel5(board: Piece[], color: number): Piece[] {
   const piece2Index = findIndexAndRemove(playerPiecesBoard, 2);
   const piece3Index = findIndexAndRemove(playerPiecesBoard, 2);
   const piece4Index = findIndexAndRemove(playerPiecesBoard, 3);
-
   const piece5Index = findIndexAndRemove(playerPiecesBoard, 3);
 
   [board[27], board[piece1Index]] = [board[piece1Index], board[27]];
   [board[26], board[piece2Index]] = [board[piece2Index], board[26]];
   [board[21], board[piece3Index]] = [board[piece3Index], board[21]];
   [board[20], board[piece4Index]] = [board[piece4Index], board[20]];
-
   [board[15], board[piece5Index]] = [board[piece5Index], board[15]];
 
   return board;
 }
 
-function createPlayerPiecesBoard(board: Piece[], color: number): Piece[] {
-  const playerPieces = board.map(piece => {
-    if (piece.color === color) {
-      return piece;
-    }
-    return EMPTY_FIELD;
-  })
+function createPlayerPiecesBoard(board: Piece[], color: number): PlayerPieces[] {
+  let playerPieces = board
+    .map((piece, index) => {
+      return {
+        pieceIndex: index,
+        piece: piece,
+      }
+    })
+    .filter(playerPiece => playerPiece.piece.color === color);
+
+  playerPieces = shufflePlayerPiecesSeededRandom(playerPieces);
 
   return playerPieces;
 }
 
-function findIndexAndRemove(playerPiecesBoard: Piece[], pieceType: number): number {
-  const index = playerPiecesBoard.findIndex(piece => {
-    if (piece) {
-      return piece.pieceType === pieceType;
-    }
-  });
+function findIndexAndRemove(playerPiecesBoard: PlayerPieces[], pieceType: number): number {
+  const index = playerPiecesBoard.findIndex(playerPiece => playerPiece.piece.pieceType === pieceType);
 
-  playerPiecesBoard[index] = EMPTY_FIELD;
+  playerPiecesBoard[index] = {
+    ...playerPiecesBoard[index],
+    piece: EMPTY_FIELD
+  };
 
-  return index;
+  return playerPiecesBoard[index].pieceIndex;
+}
+
+function shufflePlayerPiecesSeededRandom(board: PlayerPieces[]): PlayerPieces[] {
+  const seed = board.findIndex(playerPiece => playerPiece.piece.pieceType === 3);
+  for (let i = board.length - 1; i > 0; i--) {
+    const j = Math.floor(seededRandom(seed) * (i + 1));
+    [board[i], board[j]] = [board[j], board[i]];
+  }
+
+  return board;
+}
+
+// based on
+// https://softwareengineering.stackexchange.com/questions/260969/original-source-of-seed-9301-49297-233280-random-algorithm
+function seededRandom(seed: number): number {
+  const max = 1;
+  const min = 0;
+
+  seed = seed * 9301 + 49297 % 233280;
+  const rnd = seed / 233280.0;
+
+  return min + rnd * (max - min);
 }
